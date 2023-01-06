2b86944a4b3cd388fe07a56a4a9313968a799917
Muni Kumar <mkpvasu@uw.edu>
2023-01-05 18:07:15 -0800

    Split whole dataset into structured train and test data for model

--- train_val_test_splitdata.py
+++ train_val_test_splitdata.py
@@ -2,28 +2,46 @@
 import os
 import glob
 import torch
+import shutil
 from torch.utils import data
 
 
-class TrainValTestData():
-    def __init__(self, datasetPath: list):
-        super(TrainValTestData, self).__init__()
-        self.dataset = glob.glob(os.path.join(datasetPath, '*.jpg'))
-        self.trainRatio = 0.7
-        self.valRatio = 0.15
+class TrainValTestDataCreation:
+    def __init__(self):
+        super(TrainValTestDataCreation, self).__init__()
+        self.datasetPath = os.path.join(os.getcwd(), "data", "dataset")
+        self.finalDatasetPath = os.path.join(os.getcwd(), "data", "dataset_for_model")
+        self.subDirs = os.listdir(self.datasetPath)
+        self.trainRatio = 0.85
         self.testRatio = 0.15
 
-    def trainValTestSplit(self):
-        trainData, valData, testData = data.random_split(self.dataset, [self.trainRatio, self.valRatio, self.testRatio])
-        return trainData, valData, testData
+    def trainAndTestSplit(self):
+        if os.path.exists(self.finalDatasetPath):
+            shutil.rmtree(self.finalDatasetPath)
+        os.mkdir(self.finalDatasetPath)
 
+        trainDataPath = os.path.join(self.finalDatasetPath, "train_data")
+        testDataPath = os.path.join(self.finalDatasetPath, "test_data")
+
+        for dataDir in [trainDataPath, testDataPath]:
+            os.mkdir(dataDir)
+
+        for subDir in self.subDirs:
+            subDirImages = glob.glob(os.path.join(self.datasetPath, subDir, "*.jpg"))
+            trainSplitData, testSplitData = data.random_split(subDirImages, [self.trainRatio, self.testRatio])
+
+            trainDataFolder = os.path.join(trainDataPath, subDir)
+            os.mkdir(trainDataFolder)
+            for image in trainSplitData:
+                shutil.copy(image, trainDataFolder)
+
+            testDataFolder = os.path.join(testDataPath, subDir)
+            os.mkdir(testDataFolder)
+            for image in testSplitData:
+                shutil.copy(image, testDataFolder)
 
 def main():
-    entireDatasetPath =os.path.join(os.getcwd(), 'data', 'partitioned_images')
-    trainData, valData, testData = TrainValTestData(entireDatasetPath).trainValTestSplit()
-    print(len(trainData))
-    print(len(valData))
-    print(len(testData))
+    TrainValTestDataCreation().trainAndTestSplit()
 
 
 if __name__ == "__main__":
