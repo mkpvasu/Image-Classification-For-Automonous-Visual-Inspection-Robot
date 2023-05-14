ecb7563ca0e143df17f25b319153949f5b1820d5
Muni Kumar <92652526+mkpvasu@users.noreply.github.com>
2023-05-13 18:02:49 -0700

    Added updated files after DoE Training

---
+++ partitioning_image_ros_node.py
@@ -0,0 +1,55 @@
+import os
+import cv2
+import shutil
+
+
+class PartitionImage:
+    def __init__(self, image_path, sub_image_height=528, sub_image_width=528):
+        super(PartitionImage, self).__init__()
+        self.partitionImagesDir = None
+        self.capturedImgPath = image_path
+        self.capturedImgName = os.path.splitext(os.path.basename(image_path))[0]
+        self.rowsPImg = sub_image_height
+        self.columnsPImg = sub_image_width
+        self.setPartitionImagesPath()
+
+    # PARTITION FULL RESOLUTION IMAGE
+    def partitionImage(self):
+        
+        if not os.path.exists(self.capturedImgPath):
+            raise FileNotFoundError(f"[{self.capturedImgName}]: Doesn't Exist in the Specified Folder")
+        
+        savePartitionedImagePath = os.path.join(self.partitionImagesDir, self.capturedImgName)
+
+        # IF PARTITIONED IMAGE FOLDER ALREADY EXISTS, REMOVE IT AND CREATE NEW ONE
+        if os.path.exists(savePartitionedImagePath):
+            shutil.rmtree(savePartitionedImagePath)
+        os.makedirs(savePartitionedImagePath)
+
+        img = cv2.imread(self.capturedImgPath)
+        partitionedImgs = [img[x:x+self.rowsPImg, y:y+self.columnsPImg] for x in range(0, img.shape[0], self.rowsPImg)
+                           for y in range(0, img.shape[1], self.columnsPImg)]
+        for i in range(len(partitionedImgs)):
+            PImgName = self.capturedImgName + '_' + str(i + 1).zfill(3) + '.jpg'
+            PImgPath = os.path.join(savePartitionedImagePath, PImgName)
+            cv2.imwrite(PImgPath, partitionedImgs[i])
+
+    # FIND PATH TO SAVE SUB-IMAGES FOR EACH IMAGE BASED ON FOLDERS CONVENTIONS
+    def setPartitionImagesPath(self):
+        capturedImagePath = os.path.normpath(os.path.dirname(self.capturedImgPath))
+        capturedImagePathComponents = capturedImagePath.split(os.sep)
+        savePartitionImagePathComponents = [component.replace("Full_Resolution_Images", "Partitioned_Images")
+                                          for component in capturedImagePathComponents]
+        partitionImagePassDir = savePartitionImagePathComponents[0] + os.sep
+        savePartitionImagePathComponents.pop(0)
+        for component in savePartitionImagePathComponents:
+            partitionImagePassDir = os.path.join(partitionImagePassDir, component)
+        self.partitionImagesDir = partitionImagePassDir
+
+
+def main():
+    PartitionImage(image_path=r"G:\Macs Lab\imageClassification\data\Full_Resolution_Images\batch0\part0\30\pass_1\capt0001.jpg").partitionImage()
+
+
+if __name__ == "__main__":
+    main()
---
+++ testing_model.py
@@ -0,0 +1,115 @@
+from __future__ import print_function, division
+import os
+import json
+import pandas as pd
+import datetime as dt
+
+import torch
+from torch.utils.data import DataLoader
+from sklearn.metrics import f1_score
+from training_model import ImagesAndLabels, SandingCanopyDataset, ModelTrain
+
+
+class ModelTest:
+    def __init__(self, micron, batch_size=4):
+        # SELECT GPU IF AVAILABLE ELSE RUN IN CPU
+        self.device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+        # DEFINE BATCH SIZE FOR DATALOADER
+        self.batch_size = batch_size
+
+        # DEFINE MICRON FOR MODEL TO BE TRAINED
+        self.micron = micron
+
+        # CHANGE TEST DATA PATH ACCORDING TO MICRON SIZE
+        self.train_data_path = os.path.join(os.getcwd(), "data", "dataset_preparation", "dataset_for_model", 
+                                            self.micron, "train_set")
+        self.test_data_path = os.path.join(os.getcwd(), "data", "dataset_preparation", "dataset_for_model", 
+                                           self.micron, "test_set")
+
+        # CREATE A NEW TRAINING FOLDER EVERYTIME FOR TRAINING
+        self.training_folder = "model_training_" + dt.datetime.now().strftime("%Y-%m-%d_%H%M%S")
+        self.create_training_dir()
+
+        # DIRECTORY TO SAVE MODEL ATTRIBUTES
+        self.save_model_attributes_path = os.path.join(os.getcwd(), "model_attributes", self.micron, "next",
+                                                       self.training_folder)
+
+        # TRAIN DEEP LEARNING MODEL
+        self.model_attributes, self.trained_model = \
+            ModelTrain(train_data_path=self.train_data_path, save_model_attributes_path=self.save_model_attributes_path,
+                       micron="20_micron", n_epochs=2, batch_size=self.batch_size).output_trained_model()
+
+    def create_training_dir(self):
+        training_dir = os.path.join(os.getcwd(), "model_attributes", self.micron, "next", self.training_folder)
+        if not os.path.exists(training_dir):
+            os.makedirs(training_dir)
+
+    # TEST TRAINED MODEL WITH TESTING DATA AND TAKE THE TEST ACCURACY FOR FINAL MODEL PERFORMANCE
+    def model_test_set_accuracy(self):
+        # CONVERT IMAGES AND LABELS TO LOADABLE DATASET FOR MODEL
+        test_data_to_dataset = self.test_data_prep()
+        test_data = SandingCanopyDataset(test_data_to_dataset)
+        test_loader = DataLoader(dataset=test_data, batch_size=self.batch_size, shuffle=True, num_workers=2)
+
+        f1_predictions = []
+        f1_ground_truths = []
+        test_corrects = 0.0
+        test_data_size = len(test_data_to_dataset)
+
+        # CHANGE FROM TRAINING MODE TO EVALUATION MODE
+        self.trained_model.eval()
+
+        with torch.no_grad():
+            for images, labels in test_loader:
+                # TRANSFER IMAGES TO GPU IF AVAILABLE FOR PREDICTIONS
+                images = (images - 127.5) / 127.5
+                images = images.to(self.device)
+
+                # OUTPUT PROBABILITIES FOR ALL CLASSES
+                outputs = self.trained_model(images)
+
+                # CLASS WITH THE HIGHEST PROBABILITY WILL BE TAKEN AS FINAL PREDICTION BY MODEL
+                _, predictions = torch.max(outputs, dim=1)
+
+                # SUM OF ALL CORRECT OUTPUTS
+                test_corrects += torch.sum(predictions == labels).item()
+
+                # APPEND PREDICTIONS TO LIST
+                f1_predictions += predictions.tolist()
+
+                # APPEND GROUND TRUTHS TO LIST
+                f1_ground_truths += labels.tolist()
+
+        # OVERALL TEST ACCURACY FOR THE TEST SET
+        test_accuracy = (100 * (test_corrects / test_data_size))
+        # F1 SCORE OF ENTIRE TEST SET
+        test_f1_score = f1_score(f1_ground_truths, f1_predictions, average=None)
+
+        torch.cuda.empty_cache()
+        return test_accuracy, test_f1_score
+
+    def save_model_features(self):
+        model_accuracy, model_f1_score = self.model_test_set_accuracy()
+        # print(model_f1_score)
+
+        performance_attributes = self.model_attributes
+        # IMPORTANT FEATURES OF MODEL TO BE SAVED
+        performance_attributes["model_accuracy"] = float(model_accuracy)
+        performance_attributes["model_f1_score"] = float(model_f1_score)
+
+        with open(os.path.join(self.save_model_attributes_path, "performance.json"), "w") as saveFile:
+            json.dump(performance_attributes, saveFile, indent=2)
+
+    def test_data_prep(self):
+        test_images = ImagesAndLabels(self.test_data_path).append_images_and_labels()
+        test_data = pd.DataFrame(test_images, columns=["ImageName", "Label"])
+        return test_data
+
+
+def main():
+    ModelTest(micron="20_micron").save_model_features()
+
+
+if __name__ == "__main__":
+    main()
