04fc4457eaa97cea50f7700f2453acf459ca1c8d
Muni Kumar <mkpvasu@uw.edu>
2023-05-14 13:07:22 -0700

    Updated prediction_model.py

--- prediction_model.py
+++ prediction_model.py
@@ -1,6 +1,7 @@
 import os
 import glob
 import json
+import copy
 import pandas as pd
 from PIL import Image
 
@@ -8,78 +9,103 @@ import torch
 from torchvision import transforms
 from torch.utils.data import Dataset, DataLoader
 
+# from training_model import ModelTrain
+from torchvision.models import resnet50
+
 
 class LoadImages:
-    def __init__(self, partition_images_path):
-        self.partitionImagesPath = partition_images_path
+    def __init__(self, prediction_images_path):
+        self.prediction_images_path = prediction_images_path
 
-    def loadImagesAsDF(self):
-        predictionImages = glob.glob(os.path.join(self.partitionImagesPath, "*.jpg"))
-        predictionImagesDf = pd.DataFrame(predictionImages, columns=["ImageName"])
-        return predictionImagesDf
+    def load_images_as_dataframe(self):
+        prediction_images = glob.glob(os.path.join(self.prediction_images_path, "*.jpg"))
+        prediction_images_df = pd.DataFrame(prediction_images, columns=["ImageName"])
+        return prediction_images_df
 
 
 class PredictionDataset(Dataset):
     """
     Convert images and labels to pytorch dataset
     """
-
-    def __init__(self, dataToDataset):
-        self.data = dataToDataset
-        self.convertToTensor = transforms.Compose([transforms.ToTensor()])
+    def __init__(self, data_to_dataset):
+        self.data = data_to_dataset
+        self.mean = [0.5, 0.5, 0.5]
+        self.std = [0.5, 0.5, 0.5]
+        self.simple_transformation = transforms.Compose([transforms.Resize((1056, 1056),
+                                                                           interpolation=transforms.InterpolationMode.BICUBIC),
+                                                         transforms.ToTensor(),
+                                                         transforms.Normalize(mean=self.mean, std=self.std)])
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, idx):
         img = Image.open(self.data.loc[idx, "ImageName"])
-        image = self.convertToTensor(img)
+        image = self.simple_transformation(img)
         return image
 
 
 class ModelPredict:
-    def __init__(self, partition_images_path, current_model, batch_size=64):
+    def __init__(self, prediction_images_path, current_model, best_weights_path, batch_size=64):
         super().__init__()
-        self.partitionImagesPath = partition_images_path
-        self.batchSize = batch_size
-        self.currentModel = current_model
+        self.prediction_images_path = prediction_images_path
+        self.batch_size = batch_size
+        self.current_model = current_model
+        self.best_weights_path = best_weights_path
 
-    def prepareData(self):
-        predictionDataDf = LoadImages(self.partitionImagesPath).loadImagesAsDF()
-        predictionDataset = PredictionDataset(predictionDataDf)
-        predictionLoader = DataLoader(predictionDataset, batch_size=self.batchSize, shuffle=False, num_workers=4)
-        return predictionDataDf, predictionLoader
+    def check_paths(self):
+        if not os.path.exists(self.prediction_images_path):
+            raise FileNotFoundError(f"{self.prediction_images_path} doesn't exist")
 
-    def predictImages(self):
+        if not os.path.exists(self.best_weights_path):
+            raise FileNotFoundError(f"{self.best_weights_path} doesn't exist")
+
+    def prepare_data(self):
+        prediction_data_df = LoadImages(self.prediction_images_path).load_images_as_dataframe()
+        prediction_dataset = PredictionDataset(prediction_data_df)
+        prediction_loader = DataLoader(prediction_dataset, batch_size=self.batch_size, shuffle=False, num_workers=4)
+        return prediction_data_df, prediction_loader
+
+    def predict_images(self):
         device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-        predictionDataDf, predictionLoader = self.prepareData()
-        predictionResults = []
-        currentModel = self.currentModel.load_state_dict(torch.load("*_best-weights.pth"))
-        currentModel.to(device)
-        currentModel.eval()
+        prediction_data_df, prediction_loader = self.prepare_data()
+        prediction_results = []
+        current_model = self.current_model.load_state_dict(torch.load(self.best_weights_path, device))
+        current_model.to(device)
+        current_model.eval()
 
-        for images in predictionLoader:
+        for images in prediction_loader:
             images = images.to(device)
             with torch.no_grad():
-                outputs = currentModel(images)
+                outputs = current_model(images)
                 _, predictions = torch.max(outputs, dim=1)
-                predictionsOutput = predictions.cpu().detach().numpy()
-            for prediction in predictionsOutput:
-                predictionResults.append(prediction)
+                predictions_output = predictions.cpu().detach().numpy()
+            for prediction in predictions_output:
+                prediction_results.append(prediction)
 
-        predictionDataDf["Predictions"] = predictionResults
-        predictionsDict = dict(zip(predictionDataDf['ImageName'], predictionDataDf["Predictions"]))
+        prediction_data_df["Predictions"] = prediction_results
+        predictions_dict = dict(zip(prediction_data_df['ImageName'], prediction_data_df["Predictions"]))
 
-        return predictionsDict
+        return predictions_dict
 
-    def classifyImagesAndSavePredictions(self, classificationFilePath):
-        predictionsDict = self.predictImages()
-        with open(os.path.join(classificationFilePath, "classifications.json"), "w") as file:
-            json.dump(predictionsDict, file, indent=1)
+    def classify_images_and_save_predictions(self):
+        self.check_paths()
+        predictions_dict = self.predict_images()
+        with open(os.path.join(self.prediction_images_path, "classifications.json"), "w") as file:
+            json.dump(predictions_dict, file, indent=1)
 
 
 def main():
-    ModelPredict().classifyImagesAndSavePredictions()
+    prediction_images_path = os.path.join(os.getcwd(), "data", "dataset_preparation", "test_prediction_data",
+                                          "20_micron")
+    best_weights_path = os.path.join(os.getcwd(), "model_attributes", "20_micron", "current", "best_weights",
+                                     "best_weights.pth")
+    current_model = resnet50
+    # current_model = ModelTrain().output_model()
+
+    ModelPredict(prediction_images_path=prediction_images_path,
+                 current_model=current_model,
+                 best_weights_path=best_weights_path).classify_images_and_save_predictions()
 
 
 if __name__ == "__main__":
